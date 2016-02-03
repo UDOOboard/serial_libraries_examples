2dfcc868d1b6118710b0a3960cc7b7e796f6f9bb
Ekironji <luca.pisani@aidilab.it>
2016-02-03 16:02:26 +0000

    Java examples updated. Working with UDOObuntu 2

--- java/Java_serial_example.java
+++ java/Java_serial_example.java
@@ -27,53 +27,90 @@ import java.io.OutputStream;
 
 public class Java_serial_example {
 
-		static OutputStream out;
-		
-		public Java_serial_example() {
-				super();
-		}
-		
-		public void connect(String portName) throws Exception {
-			CommPortIdentifier mCommPortIdentifier = CommPortIdentifier.getPortIdentifier(portName);
-			if (mCommPortIdentifier.isCurrentlyOwned()) {
-				System.out.println("Error: Port is currently in use!");
-			} else {
-				CommPort mCommPort = mCommPortIdentifier.open(this.getClass().getName(), 2000);
-				if(mCommPort instanceof SerialPort) {
-					SerialPort mSerialPort = (SerialPort) mCommPort;
-					mSerialPort.setSerialPortParams(115200, 
-													SerialPort.DATABITS_8, 
-													SerialPort.STOPBITS_1,
-													SerialPort.PARITY_NONE);
-					mSerialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);							
-					
-					out = mSerialPort.getOutputStream();
-				} else {
-					System.out.println("Error: Only serial ports are handled by this example!");
-				}													
-			}
-		}
-		
+		static Serial UDOOserial;
+
 		public static void main(String[] args) {
+
+			UDOOserial = new Serial();
+
 			System.out.println("connecting to serial port...");
 			try {
-				(new Java_serial_example()).connect("/dev/ttymxc3");
+				UDOOserial.connect("/dev/ttyS0");
 				System.out.println("Connected!");
 			} catch (Exception e) {
 				e.printStackTrace();
-			}	
-			
+				System.exit(0);
+			}
+
 			try{
-				out.write('1');
+				System.out.println("write chars to Arduino...");
+				UDOOserial.writeChar('1');
 				Thread.sleep(1000);
-				out.write('0');
+				UDOOserial.writeChar('0');
 			} catch(IOException ioe) {
 				ioe.printStackTrace();
 			} catch(InterruptedException ie) {
 				ie.printStackTrace();
+			} finally {
+				UDOOserial.disconnect();
 			}
-			
-			System.exit(0);	
+
+			System.out.println("Finished!");
+			System.exit(0);
 		}
+
+
+		public static class Serial {
+
+			private SerialPort mSerialPort;
+			private OutputStream out;
 		
+			public Serial() {
+			}
+
+			public void connect(String portName) throws Exception {
+				CommPortIdentifier mCommPortIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+				if (mCommPortIdentifier.isCurrentlyOwned()) {
+					System.out.println("Error: Port is currently in use!");
+				} else {
+					CommPort mCommPort = mCommPortIdentifier.open(this.getClass().getName(), 2000);
+					if(mCommPort instanceof SerialPort) {
+						mSerialPort = (SerialPort) mCommPort;
+						mSerialPort.setSerialPortParams(115200,
+										SerialPort.DATABITS_8,
+										SerialPort.STOPBITS_1,
+										SerialPort.PARITY_NONE);
+						mSerialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
+
+						out = mSerialPort.getOutputStream();
+					} else {
+						System.out.println("Error: Only serial ports are handled by this example!");
+					}
+				}
+			}
+
+			public void writeChar(char msg) throws IOException {
+				out.write(msg);
+			}
+
+			public void disconnect() {
+				try {
+					if (out != null) {
+						out.close();
+						out = null;
+					}
+					if (mSerialPort != null) {
+						mSerialPort.close();
+						mSerialPort = null;
+					}
+				} catch (Exception e) {
+					System.out.println("Error: Port not closed!");
+				} finally {
+					out = null;
+					mSerialPort = null;
+				}
+			}
+
+		}
+
 }
--- java/Java_serial_example_bidirectional.java
+++ java/Java_serial_example_bidirectional.java
@@ -28,82 +28,36 @@ import java.io.InputStream;
 
 public class Java_serial_example_bidirectional {
 
-		static OutputStream out;
-		static InputStream in;
-		
-		public Java_serial_example_bidirectional() {
-				super();
-		}
-		
-		public void connect(String portName) throws Exception {
-			CommPortIdentifier mCommPortIdentifier = CommPortIdentifier.getPortIdentifier(portName);
-			if (mCommPortIdentifier.isCurrentlyOwned()) {
-				System.out.println("Error: Port is currently in use!");
-			} else {
-				CommPort mCommPort = mCommPortIdentifier.open(this.getClass().getName(), 2000);
-				if(mCommPort instanceof SerialPort) {
-					SerialPort mSerialPort = (SerialPort) mCommPort;
-					mSerialPort.setSerialPortParams(115200, 
-													SerialPort.DATABITS_8, 
-													SerialPort.STOPBITS_1,
-													SerialPort.PARITY_NONE);
-					mSerialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);							
-					
-					out = mSerialPort.getOutputStream();
-					in  = mSerialPort.getInputStream();
-				} else {
-					System.out.println("Error: Only serial ports are handled by this example!");
-				}													
-			}
-		}
-		
-		public static class SerialReader implements Runnable {
-			InputStream in;
-			
-			public SerialReader(InputStream in) {
-				this.in = in;
-			}
-			
-			public void run() {
-				byte[] buffer = new byte[7];
-				int len = -1;
-				String mStringReceived = "";
-				try {
-					while((len = this.in.read(buffer)) > -1) {
-						mStringReceived += new String(buffer, 0, len);
-						if (buffer[len-1] == '\n') {
-							System.out.println(mStringReceived);
-							mStringReceived = "";
-						}
-					}
-				} catch (IOException ioe) {
-					ioe.printStackTrace();
-				}
-			}
-		}
-		
+		static Serial UDOOserial;
+
 		public static void main(String[] args) {
+
+			UDOOserial = new Serial();
+
 			System.out.println("connecting to serial port...");
 			try {
-				(new Java_serial_example_bidirectional()).connect("/dev/ttymxc3");
+				UDOOserial.connect("/dev/ttyS0");
 				System.out.println("Connected!");
 			} catch (Exception e) {
 				e.printStackTrace();
-			}	
-			
+				System.exit(0);
+			}
+
 			byte[] outBuff = new byte[2];
 			
 			try{
-				(new Thread(new SerialReader(in))).start();
+				UDOOserial.read_startBufferThread();
 				outBuff[0] = (byte)13;
 				outBuff[1] = (byte)1;
-				out.write(outBuff);
+				UDOOserial.writeByteArray(outBuff);
 				
 				Thread.sleep(1000);
 				
 				outBuff[0] = (byte)13;
 				outBuff[1] = (byte)0;
-				out.write(outBuff);
+				UDOOserial.writeByteArray(outBuff);
+
+				Thread.sleep(1000);
 			} catch(IOException ioe) {
 				ioe.printStackTrace();
 			} catch(InterruptedException ie) {
@@ -113,4 +67,89 @@ public class Java_serial_example_bidirectional {
 			System.exit(0);	
 		}
 		
+
+		public static class Serial {
+
+			private SerialPort mSerialPort;
+			private OutputStream out;
+			private InputStream in;
+
+			public Serial() {
+			}
+
+			public void connect(String portName) throws Exception {
+				CommPortIdentifier mCommPortIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+				if (mCommPortIdentifier.isCurrentlyOwned()) {
+					System.out.println("Error: Port is currently in use!");
+				} else {
+					CommPort mCommPort = mCommPortIdentifier.open(this.getClass().getName(), 2000);
+					if(mCommPort instanceof SerialPort) {
+						mSerialPort = (SerialPort) mCommPort;
+						mSerialPort.setSerialPortParams(115200,
+										SerialPort.DATABITS_8,
+										SerialPort.STOPBITS_1,
+										SerialPort.PARITY_NONE);
+						mSerialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
+
+						out = mSerialPort.getOutputStream();
+						in = mSerialPort.getInputStream();
+					} else {
+						System.out.println("Error: Only serial ports are handled by this example!");
+					}
+				}
+			}
+
+			public void writeByteArray(byte[] msg) throws IOException {
+				out.write(msg);
+			}
+
+			public void read_startBufferThread() {
+				(new Thread(new SerialReader(in))).start();
+			}
+
+			public void disconnect() {
+				try {
+					if (out != null) {
+						out.close();
+						out = null;
+					}
+					if (mSerialPort != null) {
+						mSerialPort.close();
+						mSerialPort = null;
+					}
+				} catch (Exception e) {
+					System.out.println("Error: Port not closed!");
+				} finally {
+					out = null;
+					mSerialPort = null;
+				}
+			}
+
+			public static class SerialReader implements Runnable {
+
+				InputStream in;
+
+				public SerialReader(InputStream in) {
+					this.in = in;
+				}
+
+				public void run() {
+					byte[] buffer = new byte[7];
+					int len = -1;
+					String mStringReceived = "";
+					try {
+						while((len = this.in.read(buffer)) > -1) {
+							mStringReceived += new String(buffer, 0, len);
+							if (buffer[len-1] == '\n') {
+								System.out.println(mStringReceived);
+								mStringReceived = "";
+							}
+						}
+					} catch (IOException ioe) {
+						ioe.printStackTrace();
+					}
+				}
+			}
+
+		}
 }

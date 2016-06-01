f30d4afa2318b843f6c6a2848edbf5a0d162285c
Ekironji <luca.pisani@aidilab.it>
2016-06-01 08:54:47 +0000

    C: switched to linked /dev/ttyS0 serial

--- c/c_serial_example_bidirectional.c
+++ c/c_serial_example_bidirectional.c
@@ -84,7 +84,7 @@ set_blocking (int fd, int should_block)
 }
 
 int main(void) {
-	char *portname = "/dev/ttymxc3";
+	char *portname = "/dev/ttyS0";
 	
 	int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
 	if (fd < 0)
